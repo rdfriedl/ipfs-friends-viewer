0aecbee9dff62cd41bafa33bddd4e92b45f7d35a
Robert Friedl <rdfriedl@gmail.com>
2021-07-30 12:53:42 -0500

    clean up view image view

--- src/views/ViewImage.tsx
+++ src/views/ViewImage.tsx
@@ -8,6 +8,29 @@ import { decrypt, readMessage } from "openpgp";
 import { useAppSettings } from "../providers/AppSettingsProvider";
 import { useKeys } from "../providers/KeysProvider";
 
+async function readUint8Stream(stream: ReadableStream<Uint8Array>){
+	const reader = stream.getReader();
+
+	const arrays: Uint8Array[] = [];
+	let done = false;
+	while (!done) {
+		const result = await reader.read();
+		if (result.done) {
+			done = true;
+			break;
+		}
+		if (result.value) arrays.push(result.value);
+	}
+	const merged = new Uint8Array(arrays.reduce((length, arr) => length + arr.length, 0));
+	let offset = 0;
+	for (const array of arrays) {
+		merged.set(array, offset);
+		offset += array.length;
+	}
+
+	return merged;
+}
+
 type ViewImagePageProps = {
 	ipfsHash: string;
 };
@@ -30,23 +53,7 @@ export const ViewImagePage = ({ ipfsHash }: ViewImagePageProps) => {
 				format: "binary",
 			});
 
-			const reader = decrypted.data.getReader() as ReadableStreamDefaultReader<Uint8Array>;
-			const arrays: Uint8Array[] = [];
-			let done = false;
-			while (!done) {
-				const result = await reader.read();
-				if (result.done) {
-					done = true;
-					break;
-				}
-				if (result.value) arrays.push(result.value);
-			}
-			const merged = new Uint8Array(arrays.reduce((length, arr) => length + arr.length, 0));
-			let offset = 0;
-			for (const array of arrays) {
-				merged.set(array, offset);
-				offset += array.length;
-			}
+			const merged = await readUint8Stream(decrypted.data as ReadableStream<Uint8Array>);
 			const blob = new Blob([merged], { type: "image/jpeg" });
 			return URL.createObjectURL(blob);
 		},
